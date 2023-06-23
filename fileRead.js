4a1f128171c5586030f109630e3a11e6df408c59
Anjan Talatam <tspa2117@gmail.com>
2023-06-23 23:49:23 +0530

    👌 IMPROVE: remove multiple git repos

--- fileRead.js
+++
@@ -1,5 +0,0 @@
-import fs from "fs";
-
-fs.readFile("a.txt", "utf-8", (err, data) => {
-  console.log(data);
-});
--- middleware/requestCounter.js
+++
@@ -1,7 +0,0 @@
-let count = 0;
-
-export default function requestCounter(req, res, next) {
-  count++;
-  console.log(count, "count");
-  next();
-}
