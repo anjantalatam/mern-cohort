64704335bb76c8484ea6c6bd45a64614bbf51092
Anjan Talatam <tspa2117@gmail.com>
2023-06-24 13:01:07 +0530

    📦 NEW: authenticationServer: user Login

--- 02-nodejs/authenticationServer.js
+++ 02-nodejs/authenticationServer.js
@@ -67,6 +67,23 @@ app.post("/signup", (req, res) => {
   res.status(201).send("Signup successful");
 });
 
+// 2. POST /login - User Login
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find((u) => u.email === email);
+
+  if (!user || user.password !== password) {
+    res.status(401).send("Invalid credentials");
+  }
+
+  res.send({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+  });
+});
+
 // Dev Mode
 app.listen(PORT, () => {
   console.log(`Listening on Port ${PORT}, http://localhost:${PORT}`);
