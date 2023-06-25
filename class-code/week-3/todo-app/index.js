1e5897ee795092baa643e94d44c54e372276dd10
Anjan Talatam <tspa2117@gmail.com>
2023-06-25 16:24:20 +0530

    📖 DOC: Add comments

--- class-code/week-3/todo-app/index.js
+++ class-code/week-3/todo-app/index.js
@@ -1,34 +1,41 @@
-// DOM
+// Create Todo Card and Append it to div with class todo-list
 function makeTodoCard(title, description, id) {
+  // create div with todo-item class
   const todoItem = document.createElement("div");
   todoItem.classList.add("todo-item");
 
+  // create title and description <span> elements
   const todoItemTitle = document.createElement("span");
   const todoItemDescription = document.createElement("span");
 
+  // add classes to title and description <span> elements
   todoItemTitle.classList.add("todo-item-title");
   todoItemDescription.classList.add("todo-item-description");
 
+  // Add value to title and description <span> elements
   todoItemTitle.innerText = title;
   todoItemDescription.innerText = description;
 
   const todoItemActions = document.createElement("div");
   todoItemActions.classList.add("todo-item-actions");
 
+  // create Edit button and add the class and text
   const todoEdit = document.createElement("button");
   todoEdit.classList.add("todo-edit");
   todoEdit.innerText = "Edit";
 
-  // on edit click
+  // add click listener to Edit Button
   todoEdit.addEventListener("click", () => editTodo(id));
 
+  // create Delete button and add the class and text
   const todoDelete = document.createElement("button");
   todoDelete.classList.add("todo-delete");
   todoDelete.innerText = "Delete";
 
-  // on delete click
+  // add click listener to Delete Button
   todoDelete.addEventListener("click", () => deleteTodo(id));
 
+  // append (push) to respective nodes
   todoItemActions.appendChild(todoEdit);
   todoItemActions.appendChild(todoDelete);
 
@@ -36,6 +43,7 @@ function makeTodoCard(title, description, id) {
   todoItem.appendChild(todoItemDescription);
   todoItem.appendChild(todoItemActions);
 
+  // push todo-item to todo-list
   const todoList = document.getElementById("todo-list");
   todoList.appendChild(todoItem);
 }
@@ -89,6 +97,7 @@ function editTodo(id) {
 
 function getTodos() {
   const todoList = document.getElementById("todo-list");
+  // Clear todos to remove any stale data
   todoList.innerHTML = "";
 
   fetch("http://localhost:3000/todos", {
