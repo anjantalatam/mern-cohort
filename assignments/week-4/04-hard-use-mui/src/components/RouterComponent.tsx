c14d9f090cc755dfb2416ddd17a73c16c5b1b7ff
Anjan Talatam <tspa2117@gmail.com>
2023-07-07 21:45:52 +0530

    👌 IMPROVE: week4: a4: add courses, not found routes

--- assignments/week-4/04-hard-use-mui/src/components/RouterComponent.tsx
+++ assignments/week-4/04-hard-use-mui/src/components/RouterComponent.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from '../modules/Login';
 import Signup from '../modules/Signup';
+import Courses from '../modules/Courses';
+import NotFound from './NotFound';
 
 function RouterComponent() {
   return (
     <Routes>
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/courses" element={<Courses />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
