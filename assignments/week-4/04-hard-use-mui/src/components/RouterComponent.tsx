20c89e93f7f77e9cb34ba51dc1dbfb5e16f0de4d
Anjan Talatam <tspa2117@gmail.com>
2023-07-07 17:58:42 +0530

    📦 NEW: week-4: a-4: login and signup screen with basic validations

--- assignments/week-4/04-hard-use-mui/src/components/RouterComponent.tsx
+++ assignments/week-4/04-hard-use-mui/src/components/RouterComponent.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from '../modules/Login';
+import Signup from '../modules/Signup';
 
 function RouterComponent() {
   return (
     <Routes>
+      <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
     </Routes>
   );
