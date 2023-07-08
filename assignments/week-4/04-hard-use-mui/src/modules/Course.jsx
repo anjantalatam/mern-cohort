dee9433f477a1f281fcaab1592595f6c142fe594
Anjan Talatam <tspa2117@gmail.com>
2023-07-08 20:18:53 +0530

    📦 NEW: week-4: a-4: purchase course

--- assignments/week-4/04-hard-use-mui/src/modules/Course.jsx
+++ assignments/week-4/04-hard-use-mui/src/modules/Course.jsx
@@ -7,11 +7,45 @@ import {
   Typography,
 } from '@mui/material';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useAuth, useSnackbar } from '../contexts';
+import { API_END_POINTS } from '../utility';
+import { customAxios } from '../axios';
 
 function Course() {
   const { state: course } = useLocation();
+  const { openSnackbar } = useSnackbar();
+
+  const { currentRole } = useAuth();
+
+  const isTutor = currentRole === 'admin';
 
   const navigate = useNavigate();
+
+  function purchaseCourse() {
+    const token = localStorage.getItem('token');
+    const purchase = async () => {
+      let url = `${API_END_POINTS.dev}/users/courses/${course.id}`;
+
+      try {
+        const res = await customAxios.post(
+          url,
+          {},
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        );
+
+        openSnackbar(res.data.message);
+        // redirect to purchased courses
+      } catch (e) {
+        openSnackbar(e.response.data.message);
+      }
+    };
+
+    purchase();
+  }
 
   return (
     <>
@@ -60,13 +94,25 @@ function Course() {
           <Typography variant="h4" sx={{ mt: 3 }}>
             Price: {course.price}
           </Typography>
-          <Button
-            fullWidth
-            variant="contained"
-            sx={{ mt: 5 }}
-            onClick={() => navigate('/courses/edit', { state: course })}>
-            Edit Course
-          </Button>
+          {isTutor ? (
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 5 }}
+              onClick={() =>
+                navigate('/tutor/courses/edit', { state: course })
+              }>
+              Edit Course
+            </Button>
+          ) : (
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 5 }}
+              onClick={purchaseCourse}>
+              Purchase Course
+            </Button>
+          )}
         </Container>
       </Box>
     </>
