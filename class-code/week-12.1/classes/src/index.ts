5d16e233558b8108eb106b708332bb58014a0248
Anjan Talatam <tspa2117@gmail.com>
2023-08-26 20:09:34 +0530

    decorators js

--- class-code/week-12.1/classes/src/index.ts
+++ class-code/week-12.1/classes/src/index.ts
@@ -1,30 +1,21 @@
+import { measure } from 'helpful-decorators';
+
 class DateClass {
   private timeZone: string;
   constructor(timeZone: string) {
     this.timeZone = timeZone;
   }
 
+  @measure
   getMonth() {
     const date = new Date();
+    console.log('hi');
     return date.getMonth();
-  }
-
-  expensiveOperation() {
-    const startTime = new Date().getTime();
-    let ctr = 0;
-
-    for (let i = 0; i < 10000000000; i++) {
-      ctr++;
-    }
-
-    console.log(ctr);
-    const endTime = new Date().getTime();
-    console.log(endTime - startTime, 'ms');
   }
 }
 
 const dateObject = new DateClass('IND');
-const response = dateObject.getMonth();
-const time = dateObject.expensiveOperation();
-
-console.log(time);
+dateObject.getMonth();
+dateObject.getMonth();
+dateObject.getMonth();
+dateObject.getMonth();
